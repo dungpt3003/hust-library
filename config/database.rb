64f2cb5f28aae1dc1a7d7555977b286b4470b1cb
dungpt3003 <dungpt3003@gmail.com>
2016-11-28 22:57:22 +0700

    Add config file

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,28 @@
 Sequel::Model.plugin(:schema)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
-Sequel::Model.db = case Padrino.env
-  when :development then Sequel.connect("postgres://localhost/hust_library_development", :loggers => [logger])
-  when :production  then Sequel.connect("postgres://localhost/hust_library_production",  :loggers => [logger])
-  when :test        then Sequel.connect("postgres://localhost/hust_library_test",        :loggers => [logger])
-end
+
+#
+# Config your Postgresql connection using the following environment variables
+# in production and testing environment:
+# * PG_USERNAME, e.g. dungpt
+# * PG_PASSWORD, e.g. abcabc
+# * PG_HOST, e.g. localhost:3000
+# * PG_PORT, e.g. 3000
+# * LIBRARY_DB: name of the database, e.g. hust-library
+#
+
+DB = Sequel::Model.db =
+  case Padrino.env
+  # when :development
+  #   Sequel.connect "sqlite://bayo-dev.db"
+  when :production, :test, :development
+    Sequel.connect(
+      :adapter  => 'postgres',
+      :host     => "#{ENV['PG_HOST']}",
+      :port     => "#{ENV['PG_PORT']}",
+      :user     => "#{ENV['PG_USERNAME']}",
+      :password => "#{ENV['PG_PASSWORD']}",
+      :database => "#{ENV['LIBRARY_DB']}",
+      :loggers  => [logger]
+    )
+  end
