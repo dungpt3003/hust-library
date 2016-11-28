472257c7124616b59354ae809a43ea94b66bb36a
dungpt3003 <dungpt3003@gmail.com>
2016-11-28 23:05:42 +0700

    Migration: Create table libraries

--- db/migrate/001_create_libraries.rb
+++ db/migrate/001_create_libraries.rb
@@ -2,7 +2,13 @@ Sequel.migration do
   up do
     create_table :libraries do
       primary_key :id
-      
+      String :name
+      String :address
+      String :phone
+      Float :p_access
+      Float :p_access_point
+      Float :p_query
+      Float :p_megabyte
     end
   end
 
